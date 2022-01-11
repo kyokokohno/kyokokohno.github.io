1817d44b5d3b15b2de065278b837ff2f88d1dcf6
Kyoko Kohno <kyokokohno@gmail.com>
2022-01-11 02:26:42 -0600

    made navbar and img responsive, added footer

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-
-function validateForm() {
-    let x = "index.html".contactform ["myForm"]["firstname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
   }
